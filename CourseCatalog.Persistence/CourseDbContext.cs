6d5738faf8b4590cfe436199100f13c322cd10e5
Mark Lawrence <mlawrence@alsde.edu>
2021-02-22 10:41:21 -0600

    added persistence library

---
+++ CourseCatalog.Persistence/CourseDbContext.cs
@@ -0,0 +1,21 @@
+﻿using CourseCatalog.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace CourseCatalog.Persistence
+{
+    public class CourseDbContext : DbContext
+    {
+
+        public DbSet<Course> Courses { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            var _connectionString = "Data Source=.;initial catalog=Courses;Integrated Security=True;Connect Timeout=15;Encrypt=False;TrustServerCertificate=False;MultipleActiveResultSets=true;Application Name=Courses";
+            optionsBuilder
+                .UseSqlServer(_connectionString)
+                .EnableSensitiveDataLogging()
+                //.UseLazyLoadingProxies(_useProxyLoading)
+                ;
+        }
+    }
+}
