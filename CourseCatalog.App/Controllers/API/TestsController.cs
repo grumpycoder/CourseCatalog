d9f8b42025b1201d1b4096fe1bb241eae7175a1a
Mark Lawrence <mlawrence@alsde.edu>
2021-03-12 13:00:42 -0600

    added Test api controller

---
+++ CourseCatalog.App/Controllers/API/TestsController.cs
@@ -0,0 +1,29 @@
+﻿using System.Threading.Tasks;
+using System.Web.Http;
+
+namespace CourseCatalog.App.Controllers.API
+{
+    [RoutePrefix("tests")]
+    public class TestsController : ApiController
+    {
+
+        [HttpGet, Route("")]
+        public async Task<IHttpActionResult> Get()
+        {
+            return Ok("Get Request");
+        }
+
+        [HttpPut, Route("")]
+        public async Task<IHttpActionResult> Put()
+        {
+            return Ok("Put Request");
+        }
+
+        [HttpDelete, Route("")]
+        public async Task<IHttpActionResult> Delete()
+        {
+            return Ok("Delete Request");
+        }
+
+    }
+}
